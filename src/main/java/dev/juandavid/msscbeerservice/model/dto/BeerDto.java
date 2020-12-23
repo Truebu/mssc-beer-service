785deef96dfc1e102e6d41ca148ef999f8f3bf94
Truebu <juan.david.casta.2801@gmail.com>
2020-12-23 17:32:10 -0500

    beerController created

--- src/main/java/dev/juandavid/msscbeerservice/model/dto/BeerDto.java
+++ src/main/java/dev/juandavid/msscbeerservice/model/dto/BeerDto.java
@@ -1,11 +1,19 @@
 package dev.juandavid.msscbeerservice.model.dto;
 
 import dev.juandavid.msscbeerservice.model.constants.EBeerStyle;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BeerDto {
     private UUID id;
     private Integer version;
