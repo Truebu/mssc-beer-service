ee69f136ebc455ebb1ddc4975b64662e964bb322
Truebu <juan.david.casta.2801@gmail.com>
2021-01-18 18:34:08 -0500

    build correct project

--- src/main/java/dev/juandavid/msscbeerservice/model/repositories/BeerRepository.java
+++ src/main/java/dev/juandavid/msscbeerservice/model/repositories/BeerRepository.java
@@ -1,12 +1,11 @@
 package dev.juandavid.msscbeerservice.model.repositories;
 
 import dev.juandavid.msscbeerservice.model.entities.Beer;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
 
 import java.util.UUID;
 
-@Repository
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID> {
 
 }
